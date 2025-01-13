84a14d26b847639844c5394e48000dac3a6c91d6
BanesaruDenisa <77294618+BanesaruDenisa@users.noreply.github.com>
2025-01-13 17:04:32 +0200

    test

--- photobooking/photobooking/src/main/java/com/example/photobooking/model/entities/DTO/BookingDTO.java
+++ photobooking/photobooking/src/main/java/com/example/photobooking/model/entities/DTO/BookingDTO.java
@@ -4,7 +4,6 @@ import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
-import lombok.AllArgsConstructor;
 
 import java.time.LocalDate;
 
